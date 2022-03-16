4a4e6485efa01f35347de9b2ddf01fefe888488d
JOMAR NACORDA <jnacorda04@gmail.com>
2022-03-16 14:03:32 +0800

    Init Commit

---
+++ gui/gui.py
@@ -0,0 +1,16 @@
+from tkinter import Canvas, Label, Tk, PhotoImage
+
+
+root = Tk()
+root.title("tk")
+
+canvas = Canvas(root, width=400, height=400)
+
+gif = PhotoImage(file="images/giphy.gif")
+gif_lbl = Label(root, image=gif)
+gif_lbl.grid (row=0, column=0)
+
+text =  Label(text="jomar A. Nacorda\n Davao City, April 13,2003", pady=10, padx=10, bg="light blue", fg="orange", font=("Arial", 12))
+text.grid(row=0, column=1)
+
+root.mainloop()
