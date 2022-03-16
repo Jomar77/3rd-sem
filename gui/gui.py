282f3044a7cb3e025e0887981db9cef04ed1c7b0
JOMAR NACORDA <jnacorda04@gmail.com>
2022-03-16 14:10:16 +0800

    color change

--- gui/gui.py
+++ gui/gui.py
@@ -10,7 +10,7 @@ gif = PhotoImage(file="images/giphy.gif")
 gif_lbl = Label(root, image=gif)
 gif_lbl.grid (row=0, column=0)
 
-text =  Label(text="jomar A. Nacorda\n Davao City, April 13,2003", pady=10, padx=10, bg="light blue", fg="orange", font=("Arial", 12))
+text =  Label(text="Jomar A. Nacorda\n Davao City, April 13,2003", pady=10, padx=10, bg="light blue", fg="orange red", font=("Arial", 12))
 text.grid(row=0, column=1)
 
 root.mainloop()
